bc88f871979d3f0b78e3b91ae672877066ebe160
sherg <vikram7ce@gmail.com>
2024-07-12 12:14:24 +0530

    Sheryl changed again

--- src/main/java/com/training/bms/AlstromApp2Application.java
+++ src/main/java/com/training/bms/AlstromApp2Application.java
@@ -9,6 +9,7 @@ public class AlstromApp2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(AlstromApp2Application.class, args);
 		System.out.println("Hello BY SHERYL");
+		System.out.println("ON Jenkins now!");
 	}
 
 }
