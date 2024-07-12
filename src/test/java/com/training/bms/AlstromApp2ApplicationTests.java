9fbac1fb817797f21cf462f94c8e33041597b275
sherg <vikram7ce@gmail.com>
2024-07-12 12:36:23 +0530

    Sheryl updated TEST

--- src/test/java/com/training/bms/AlstromApp2ApplicationTests.java
+++ src/test/java/com/training/bms/AlstromApp2ApplicationTests.java
@@ -2,12 +2,14 @@ package com.training.bms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AlstromApp2ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(190, 200);
 	}
 
 }
