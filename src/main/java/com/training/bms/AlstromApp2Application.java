32268db420d25dc61e217b95eeab6e516acb04c1
sherg <vikram7ce@gmail.com>
2024-07-12 09:55:23 +0530

    first commit

---
+++ src/main/java/com/training/bms/AlstromApp2Application.java
@@ -0,0 +1,14 @@
+package com.training.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlstromApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlstromApp2Application.class, args);
+		System.out.println("Hello");
+	}
+
+}
